b580c2d648ef2dc9730218c78d589e52a93ce017
Jeremy Simpson <jeremy@accur8software.com>
2021-08-23 13:28:40 -0400

    Fibonacci

---
+++ src/test/scala/hackerrank/demo/FibonacciTest.scala
@@ -0,0 +1,29 @@
+package hackerrank.demo
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.math._
+
+class FibonacciTest extends AnyFunSuite {
+
+
+  def testModule(n: Long, modulo: Long, expected: Long): Unit = {
+    test(s"Fibonacci with modulo for ${n}") {
+      assertResult(expected)(Fibonacci(n, modulo))
+    }
+  }
+
+  val module: Long = math.pow(10, 8).toInt + 7
+
+  testModule(0, module, 0)
+
+  testModule(1, module, 1)
+
+  testModule(5, module, 5)
+
+  testModule(10, module, 55)
+
+  testModule(100, module, 24278230)
+
+
+}
