fdfdc019107bf7dd03f8234933f65fb1e8d21c3c
Jeremy Simpson <jeremy@accur8software.com>
2021-08-28 13:13:11 -0400

    SubstringSearching mostly working except case 5 - 3

---
+++ src/main/scala/hackerrank/demo/SubstringSearching.scala
@@ -0,0 +1,69 @@
+package hackerrank.demo
+
+object SubstringSearching {
+
+  import scala.io.StdIn._
+  import scala.annotation.tailrec
+
+  def main(args: Array[String]): Unit = {
+
+    val inputStr = """4
+                     |abcdef
+                     |def
+                     |computer
+                     |muter
+                     |stringmatchingmat
+                     |ingmat
+                     |videobox
+                     |videobox""".stripMargin
+
+    val lines = inputStr.split("\n")
+
+    val numOfCases = lines.head.toInt
+
+    (1 to numOfCases).foreach { i =>
+      val text = lines(i)
+      val pat = lines(i + 1)
+      printMsg(text, pat)
+    }
+  }
+
+
+
+  def printMsg(text: String, pattern: String): Unit = {
+    if ( hasPattern(text, pattern) ) {
+      println("YES")
+    } else {
+      println("NO")
+    }
+  }
+
+  def hasPattern(text: String, pattern: String): Boolean = {
+    lazy val fullPattern = pattern.toList
+    @tailrec
+    def r(chars: List[Char], pat: List[Char]): Boolean = {
+      chars match {
+        case Nil =>
+          if ( pat.isEmpty )
+            true
+          else
+            false
+        case ch :: cTail =>
+          pat match {
+            case Nil =>
+              true
+            case ph :: pTail =>
+              if ( ph == ch ) {
+                r(cTail, pTail)
+              } else {
+                r(cTail, fullPattern)
+              }
+          }
+      }
+    }
+    r(text.toList, fullPattern)
+  }
+
+
+
+}
